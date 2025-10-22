2be4475e6ec646e989f6ca276ec310f4053f6935
Dominic Chen <cchendominic@gmail.com>
2025-10-22 14:54:19 -0400

    Add chrome packages and logic to input slider
    Change the DOM so that the value the slider is at shows up on the page

---
+++ src/options.ts
@@ -0,0 +1,55 @@
+// Slowdown Bar
+
+type Speeds = { fastSpeed: number; slowSpeed: number };
+const DEFAULTS: Speeds = { fastSpeed: 2.0, slowSpeed: 1.0 };
+
+const fastSlider = document.getElementById("fast-slider") as HTMLInputElement | null;
+const slowSlider = document.getElementById("slow-slider") as HTMLInputElement | null;
+const fastLabel = document.getElementById("fast-label") as HTMLElement | null;
+const slowLabel = document.getElementById("slow-label") as HTMLElement | null;
+const saveBtn = document.getElementById("save-btn") as HTMLButtonElement | null;
+const resetBtn = document.getElementById("reset-btn") as HTMLButtonElement | null;
+
+const fmt = (x: number) => `${x.toFixed(1)}x`;
+const setLabels = (f: number, s: number) => {
+    if (fastLabel) fastLabel.textContent = fmt(f);
+    if (slowLabel) slowLabel.textContent = fmt(s);
+};
+
+fastSlider?.addEventListener("input", () => {
+    setLabels(Number(fastSlider.value), Number(slowSlider?.value ?? DEFAULTS.slowSpeed));
+});
+
+slowSlider?.addEventListener("input", () => {
+    setLabels(Number(fastSlider?.value ?? DEFAULTS.fastSpeed),Number(slowSlider.value))
+})
+
+
+
+
+
+// fastRange.addEventListener("input", () => {
+//     const fastValue = fastRange.value;
+//     fastLabel.textContent = `${fastValue}`;
+
+// })
+
+
+
+// const load = () => {
+//     chrome.storage.sync.get(["fastSpeed", "slowSpeed"], (data: Partial<Speeds>)) => {
+//     const fast = Number.isFinite(data.fastSpeed) && (data.fastSpeed as number)! > 0 ? (data.fastSpeed as number) : DEFAULTS.fastSpeed;
+//     const slow = Number.isFinite(data.slowSpeed) && (data.slowSpeed as number)! > 0 ? (data.slowSpeed as number) : DEFAULTS.slowSpeed;
+
+//     }
+// }
+
+// const readUI = () => {
+
+// }
+
+// const save = () => {
+//     const cfg = readUI();
+//     chrome.strogage.sync.set(cfg);
+//     setLabels(cfg.fastSpeed, cfg.slowSpeed);
+// }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        options: "src/options.ts",
+        content: "src/content.ts"
+      },
+      output: {
+        entryFileNames: `[name].js`
+      },
+    },
+  },
 })
